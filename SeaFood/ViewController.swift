908844d1e920a3e0738f060ce3f3e03d7148fbaf
Oğuz <oguzkaratoruk@yandex.com>
2018-12-14 13:40:26 +0300

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Oğuz Karatoruk on 14.12.2018.
+//  Copyright © 2018 Oğuz Karatoruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
